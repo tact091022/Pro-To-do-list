bbb5746c28d9d697eb88348e63477f6d78d6df0e
月形拓人 <takuto.tsukigata@meetsmore.com>
2023-01-29 01:04:48 +0900

    Add todolist code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean; 
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("")
+  }
+
+  const handleEdit = () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>
+          Todoリスト with Typescript</h2>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input type="text" 
+            onChange={(e) => handleChange(e)} className="inputText"/>
+            <input type="submit" value="作成" className='submitButton'/>
+          </form>
+          <ul className="todoList">
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                <input type="text" 
+              onChange={() => handleEdit()} 
+              className="inputTÏext"
+              value={todo.inputValue}/>
+              </li>
+            ))}
+          </ul>
+      </div>
     </div>
   );
 }
